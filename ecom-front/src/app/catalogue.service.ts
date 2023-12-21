f90b2f966143f6b3603905045f044c1d3dd8e4de
tareklaaouane <tlaaouane@gmail.com>
2023-12-21 13:02:08 +0100

    show products

---
+++ ecom-front/src/app/catalogue.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CatalogueService {
+  public host:string="http://localhost:8082/"
+
+    constructor(private http:HttpClient) { }
+
+    public getResource(url: string){
+      return this.http.get(this.host+url);
+    }
+  }
--- ecom-front/src/app/products/products.component.ts
+++ ecom-front/src/app/products/products.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { CatalogueService } from './../catalogue.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
 
-}
+export class ProductsComponent implements OnInit{
+  public products: any;
+
+   constructor (private catService:CatalogueService){
+
+   }
+   public getProducts(){
+     this.catService.getResource("products").subscribe(data=>{
+       this.products = data;
+     },err=>{
+       console.log(err);
+     })
+   }
+
+   ngOnInit(): void {
+     this.getProducts();
+   }
+ }
